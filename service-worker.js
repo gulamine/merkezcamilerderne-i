5691659f2f0c7f51bd1ca604123ddf3a59a53c3c
gulamine <gulamine@gmail.com>
2026-04-18 17:19:39 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "marmaris-Merkez-camileri-Derneğiv1";
+
+const filesToCache = [
+  "index.html",
+  "camiler.html",
+  "etkinlikler.html",
+  "ezan.html",
+  "reklam.html",
+  "faydalı-linkler.html",
+  "iletişim.html",
+  "sponsorlar.html",
+
+  "hakkimizda.html"
+];
+
+// Yükleme
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// Cache’ten çalıştırma
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
